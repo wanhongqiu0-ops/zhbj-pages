3527e6fe314822d58acc28cabfc35a0ec628fbfd
wanhongqiu0-ops <wanhongqiu0@gmail.com>
2026-09-13 22:04:59 +0800

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,38 @@
+// 智慧班级管家 Service Worker：离线缓存 + 安装
+const CACHE = 'zhbj-v1';
+const CORE = [
+  './',
+  './index.html',
+  './mobile.html',
+  './conduct_form.html',
+  './violation_form.html',
+  './manifest.webmanifest',
+  './icon-192.png',
+  './icon-512.png'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(CORE)).then(() => self.skipWaiting()));
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys => Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k))))
+      .then(() => self.clients.claim())
+  );
+});
+
+self.addEventListener('fetch', e => {
+  const url = new URL(e.request.url);
+  // 网络请求（Supabase 等）与跨域直接走网络
+  if (e.request.method !== 'GET' || url.origin !== location.origin) return;
+  e.respondWith(
+    fetch(e.request)
+      .then(res => {
+        const copy = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, copy)).catch(() => {});
+        return res;
+      })
+      .catch(() => caches.match(e.request).then(m => m || caches.match('./mobile.html')))
+  );
+});
